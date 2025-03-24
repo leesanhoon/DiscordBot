800190fe23df2a657812fdd9e95f4f91a80f5228
leesanhoon <nguyentanninh123@gmail.com>
2025-03-25 00:35:42 +0700

    chore: integrate Google Generative AI for content and image generation in bot commands

--- src/services/generateImageService.js
+++ src/services/generateImageService.js
@@ -1,15 +1,11 @@
-const { GEMINI_API_KEY } = require("../config/env");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+const { GEMINI_API_KEY } = require("../config/env");
+const { MessageAttachment } = require("discord.js");
+
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-const { AttachmentBuilder } = require("discord.js");
 
-const generateContent = async (message) => {
-    const result = await model.generateContent(message);
-    return result.response.text();
-};
-
-const generateImage = async (message, msg) => {
+async function generateImage(message) {
     // Set responseModalities to include "Image" so the model can generate  an image
     const model = genAI.getGenerativeModel({
         model: "gemini-2.0-flash-exp-image-generation",
@@ -23,21 +19,22 @@ const generateImage = async (message, msg) => {
         for (const part of response.response.candidates[0].content.parts) {
             // Based on the part type, either show the text or save the image
             if (part.text) {
-                return await msg.reply(
-                    "Tôi bị cụt không vẽ được hình đâu hihi"
-                );
+                console.log(part.text);
+                return part.text;
             } else if (part.inlineData) {
                 const imageData = part.inlineData.data;
                 const buffer = Buffer.from(imageData, "base64");
-                const attachment = new AttachmentBuilder(buffer, {
-                    name: "native-image.png",
-                });
-                await msg.channel.send(attachment);
+                const attachment = new MessageAttachment(
+                    buffer,
+                    "native-image.png"
+                );
+                console.log("Image generated and ready to be sent to Discord");
+                return attachment;
             }
         }
     } catch (error) {
         console.error("Error generating content:", error);
     }
-};
+}
 
-module.exports = { generateContent, generateImage };
+// module.exports = { generateImage };
