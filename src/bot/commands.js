5879e7963c229a96706b73beaf8d487d96d378b4
leesanhoon <nguyentanninh123@gmail.com>
2025-03-25 00:38:03 +0700

    chore: refactor message handling for image and content generation commands

--- src/bot/commands.js
+++ src/bot/commands.js
@@ -1,37 +1,28 @@
 const { generateContent, generateImage } = require("../services/geminiService");
 
 const handleMessage = async (client, msg) => {
-    if (msg.content.includes(`<@${client.user.id}>`)) {
-        if (msg.content.includes("!img")) {
-            const query = msg.content
-                .replace(`<@${client.user.id}>`, "")
-                .replace("!img", "")
-                .trim();
-            try {
-                await generateImage(query, msg);
-            } catch (error) {
-                console.error("Tôi bị ngu không trả lời dược đâu hihi", error);
-                msg.reply("Tôi bị ngu không trả lời dược đâu hihi");
-            }
+    if (!msg.content.includes(`<@${client.user.id}>`)) return;
+
+    const isImageRequest = msg.content.includes("!img");
+    const query = msg.content
+        .replace(`<@${client.user.id}>`, "")
+        .replace(isImageRequest ? "!img" : "", "")
+        .trim();
+
+    try {
+        if (isImageRequest) {
+            await generateImage(query, msg);
         } else {
-            const query = msg.content
-                .replace(`<@${client.user.id}>`, "")
-                .trim();
-            try {
-                const geminiResponse = await generateContent(query);
-                const maxLength = 2000;
-                for (let i = 0; i < geminiResponse.length; i += maxLength) {
-                    const replyChunk = geminiResponse.substring(
-                        i,
-                        i + maxLength
-                    );
-                    await msg.reply(replyChunk);
-                }
-            } catch (error) {
-                console.error("Tôi bị ngu không trả lời dược đâu hihi", error);
-                msg.reply("Tôi bị ngu không trả lời dược đâu hihi");
+            const geminiResponse = await generateContent(query);
+            const maxLength = 2000;
+            for (let i = 0; i < geminiResponse.length; i += maxLength) {
+                const replyChunk = geminiResponse.substring(i, i + maxLength);
+                await msg.reply(replyChunk);
             }
         }
+    } catch (error) {
+        console.error("An error occurred while processing the request", error);
+        msg.reply("Tôi bị ngu không trả lời được");
     }
 };
 
